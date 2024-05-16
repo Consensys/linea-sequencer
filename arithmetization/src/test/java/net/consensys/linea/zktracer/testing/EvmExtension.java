38b84bec321a675bcee52f1b93aa962adfbcb565
Ameziane H <ameziane.hamlat@consensys.net>
2024-05-16 15:38:05 +0200

    Fix unit and reference tests
    Signed-off-by: Ameziane H <ameziane.hamlat@consensys.net>

--- arithmetization/src/test/java/net/consensys/linea/zktracer/testing/EvmExtension.java
+++ arithmetization/src/test/java/net/consensys/linea/zktracer/testing/EvmExtension.java
@@ -15,17 +15,8 @@
 
 package net.consensys.linea.zktracer.testing;
 
-import net.consensys.linea.zktracer.opcode.OpCodes;
-import org.junit.jupiter.api.extension.BeforeAllCallback;
-import org.junit.jupiter.api.extension.ExtensionContext;
-
 /**
  * A JUnit {@link org.junit.jupiter.api.extension.Extension} handling the test lifecycle for EVM
  * tests.
  */
-public class EvmExtension implements BeforeAllCallback {
-  @Override
-  public void beforeAll(ExtensionContext context) {
-    OpCodes.load();
-  }
-}
+public class EvmExtension {}
