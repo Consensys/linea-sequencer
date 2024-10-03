3f21337ebabc072c7f376fe6e7cd6f90b4ec40dd
Usman Saleem <usman@usmans.info>
2024-10-03 16:17:14 +1000

    Add rej tx reporting to all tx pool validators

--- sequencer/src/main/java/net/consensys/linea/sequencer/txpoolvalidation/LineaTransactionPoolValidatorFactory.java
+++ sequencer/src/main/java/net/consensys/linea/sequencer/txpoolvalidation/LineaTransactionPoolValidatorFactory.java
@@ -80,10 +80,11 @@ public class LineaTransactionPoolValidatorFactory implements PluginTransactionPo
   public PluginTransactionPoolValidator createTransactionValidator() {
     final var validators =
         new PluginTransactionPoolValidator[] {
-          new AllowedAddressValidator(denied),
-          new GasLimitValidator(txPoolValidatorConf),
-          new CalldataValidator(txPoolValidatorConf),
-          new ProfitabilityValidator(besuConfiguration, blockchainService, profitabilityConf),
+          new AllowedAddressValidator(denied, rejectedTxJsonRpcManager),
+          new GasLimitValidator(txPoolValidatorConf, rejectedTxJsonRpcManager),
+          new CalldataValidator(txPoolValidatorConf, rejectedTxJsonRpcManager),
+          new ProfitabilityValidator(
+              besuConfiguration, blockchainService, profitabilityConf, rejectedTxJsonRpcManager),
           new SimulationValidator(
               blockchainService,
               transactionSimulationService,
