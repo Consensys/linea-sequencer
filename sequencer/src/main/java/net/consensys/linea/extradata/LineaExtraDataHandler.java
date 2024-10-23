4e65a52100b0f8537e1734b21ffc815db4cb9725
Ade Lucas <ade.lucas@consensys.net>
2024-10-23 08:40:10 -0400

    Rename minGasPrice to ethGasPrice from the extraData.
    Signed-off-by: Ade Lucas <ade.lucas@consensys.net>

--- sequencer/src/main/java/net/consensys/linea/extradata/LineaExtraDataHandler.java
+++ sequencer/src/main/java/net/consensys/linea/extradata/LineaExtraDataHandler.java
@@ -94,8 +94,7 @@ public class LineaExtraDataHandler {
    *
    * <p>Version 1 has this format:
    *
-   * <p>VERSION (1 byte) FIXED_COST (4 bytes) VARIABLE_COST (4 bytes) MIN_GAS_PRICE (4 bytes)
-   * ETH_GAS_PRICE (4 bytes)
+   * <p>VERSION (1 byte) FIXED_COST (4 bytes) VARIABLE_COST (4 bytes) ETH_GAS_PRICE (4 bytes)
    */
   @SuppressWarnings("rawtypes")
   private class Version1Consumer implements ExtraDataConsumer {
@@ -115,16 +114,11 @@ public class LineaExtraDataHandler {
       final FieldConsumer variableGasCostField =
           new FieldConsumer<>(
               "variableGasCost", 4, ExtraDataConsumer::toLong, currVariableCostKWei::setValue);
-      final FieldConsumer minGasPriceField =
-          new FieldConsumer<>("minGasPrice", 4, ExtraDataConsumer::toLong, this::updateMinGasPrice);
       final FieldConsumer ethGasPriceField =
-          new FieldConsumer<>(
-              "ethGasPrice", 4, ExtraDataConsumer::toLong, currEthGasPriceKWei::setValue);
+          new FieldConsumer<>("ethGasPrice", 4, ExtraDataConsumer::toLong, this::updateEthGasPrice);
 
       this.fieldsSequence =
-          new FieldConsumer[] {
-            fixedGasCostField, variableGasCostField, minGasPriceField, ethGasPriceField
-          };
+          new FieldConsumer[] {fixedGasCostField, variableGasCostField, ethGasPriceField};
     }
 
     public boolean canConsume(final Bytes rawExtraData) {
@@ -145,9 +139,10 @@ public class LineaExtraDataHandler {
           currEthGasPriceKWei.longValue() * WEI_IN_KWEI);
     }
 
-    void updateMinGasPrice(final Long minGasPriceKWei) {
+    void updateEthGasPrice(final Long ethGasPriceKWei) {
+      currEthGasPriceKWei.setValue(ethGasPriceKWei);
       if (profitabilityConf.extraDataSetMinGasPriceEnabled()) {
-        final var minGasPriceWei = Wei.of(minGasPriceKWei).multiply(WEI_IN_KWEI);
+        final var minGasPriceWei = Wei.of(ethGasPriceKWei).multiply(WEI_IN_KWEI);
         final var resp =
             rpcEndpointService.call(
                 "miner_setMinGasPrice", new Object[] {minGasPriceWei.toShortHexString()});
